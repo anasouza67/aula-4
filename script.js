6c0144a3330afecef772b3395522f2662ecff03e
anasouza67 <ana.assuncao.souza@escola.pr.gov.br>
2026-08-04 10:10:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#81D4FA"; // Muda a cor ao curtir
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "#B3E5FC"; // Volta a cor original
+        }
+    }
+});
